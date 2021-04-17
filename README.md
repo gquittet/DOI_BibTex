f0ceaa7ec3fdba5b84db716aa4df848396ffea38
Watin <valentde@hotmail.com>
2021-04-17 18:57:28 +0200

    🖥️ First Commit

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# DOI_BibTex
+<h1 align="center">
+  🚀 CLI BibTex Generator using DOI API 🖥️</br>
+</h1>
+
+<p align="center">
+  <a href="#">
+    <img src="https://img.shields.io/github/license/GitWatin/DOI_BibTex" alt="Licence">
+  </a>
+
+
+  <a href="https://www.paypal.me/valentindenis  " target="blank">
+    <img src="https://img.shields.io/badge/Donate-PayPal-green.svg" alt="Donate">
+  </a>
+  </br>
+  </br>
+  <a href="mailto:github@valdenis.be">
+    <img src="https://img.shields.io/badge/Contact%20me-github%40valdenis.be-informational" alt="ContactMe">
+  </a>
+<p>
+
+---
+
+
+
+
+## Requirements
+
+✔️ Compiler : Python 3.9
+
+#### Pip install
+
+```bash
+pip install requests calendar pyperclip
+```
+
+## Utilisation
+
+This script directly retrieves the metadata of an article using the API of www.doi.org
+
+Just start the script and paste the url when asked
+Once the script is finished, the text is copied directly into the clipboard
---
+++ main.py
@@ -0,0 +1,63 @@
+import requests
+import json
+import time
+import calendar
+import pyperclip
+import locale
+locale.setlocale(locale.LC_TIME,'fr_FR')
+
+
+def Logger(level,message):
+    print("["+str(level)+" - "+ str(time.strftime('%d/%m/%Y %H:%M:%S'))+"] : "+str(message))
+
+
+def BibTexGenerator(json):
+    ## URL
+    URL ="howpublished={\\url{"+ JSON_LOAD['URL']+ "} },"
+    
+    ### Author  
+    AUTHORS = " and ".join([f"{author['family']}{','} {author['given']}" for author in JSON_LOAD["author"]])
+    AUTHORS = "author={"+AUTHORS+"},"
+    
+    ### Title
+    TITLE = "title={"+JSON_LOAD['title']+"},"
+
+    ### Year/Month
+    temp_date=JSON_LOAD['created']
+    temp_date=temp_date['date-parts'][0]
+    DATE="month={"+str(calendar.month_name[int(temp_date[1])])+"},\n"+"year={"+str(temp_date[0])+"},"
+
+
+    ## ID
+    KEY = str(JSON_LOAD["prefix"])+"_"+str(temp_date[1])+"_"+str(temp_date[0])
+
+    ## Note
+    NOTE ="note={ [En Ligne] - consulté le "+ time.strftime('%d') +" "+ calendar.month_name[int(time.strftime("%m"))] + " "+ time.strftime("%Y")+" }"
+
+    ## Final display
+    FINAL="\n@misc{"+KEY+",\n"+AUTHORS+"\n"+TITLE+"\n"+URL+"\n"+DATE+"\n"+NOTE+"}\n"
+
+    pyperclip.copy(FINAL)
+    Logger("INFO","BibTex copy to your clipboard")
+
+
+print("Enter DOI Number Only URL")
+DOI_URL = input()
+Logger("INFO", "Wait for DOI Lookup")
+
+
+## GET JSON file
+PARAMETER={'Accept': 'application/vnd.citationstyles.csl+json'}
+
+DOI_REQUEST = requests.get(DOI_URL,headers=PARAMETER)
+JSON_DOI=DOI_REQUEST.text
+
+if(DOI_REQUEST.status_code==200):
+    Logger("INFO", "HTTP Status Code :"+str(DOI_REQUEST.status_code))
+    JSON_LOAD = json.loads(JSON_DOI)
+    BibTexGenerator(JSON_LOAD)
+
+else:
+    Logger("ERROR","Something Wrong")
+
+
